dcdc689b95fee7d4f4136e2634320d74d2192e74
daw2-cano22 <canoquintanaadria@fpllefia.com>
2021-10-21 16:53:51 +0200

    Versió 0.2-A del projecte prjava43cognom22. Branca00 creada i modificada.

--- src/prjava43cano22/Prjava43cano22.java
+++ src/prjava43cano22/Prjava43cano22.java
@@ -35,8 +35,8 @@ public class Prjava43cano22 extends Frame
         // "this" Frame fires WindowEvent its registered WindowEvent listener
         // "this" Frame adds "this" object as a WindowEvent listener
 
-        setTitle("prjava43cognom22");  // "this" Frame sets title
-        setSize(250, 100);        // "this" Frame sets initial size
+        setTitle("nova finestra prjava43cano22");  // "this" Frame sets title
+        setSize(450, 100);        // "this" Frame sets initial size
         setVisible(true);         // "this" Frame shows
     }
 
