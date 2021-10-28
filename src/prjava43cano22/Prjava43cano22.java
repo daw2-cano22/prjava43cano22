3ab4baef53bb907a72105a76ec69f8181a82b711
daw2-cano22 <canoquintanaadria@fpllefia.com>
2021-10-28 17:34:23 +0200

    Versió 0.3-B del projecte prjava43cognom22. Branca00 modificada

--- src/prjava43cano22/Prjava43cano22.java
+++ src/prjava43cano22/Prjava43cano22.java
@@ -2,6 +2,8 @@ package prjava43cano22;
 
 import java.awt.*;        // using AWT containers and components
 import java.awt.event.*;  // using AWT events and listener interfaces
+import java.awt.event.*;  // using AWT events and listener interfaces
+import java.io.*;
 
 // An AWT GUI program inherits the top-level container java.awt.Frame
 public class Prjava43cano22 extends Frame
